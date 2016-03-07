f81fa21379a3f4bc85ca93018b28766139699ab3
Mitchell Porter <mitchellporter@gmail.com>
2016-03-06 22:02:34 -0800

    new logic for max up and down animation from original center + comments

--- PanTesting/ViewController.swift
+++ PanTesting/ViewController.swift
@@ -12,22 +12,42 @@ import MotionAnimation
 class ViewController: UIViewController, UIGestureRecognizerDelegate {
     
     @IBOutlet weak var redView: UIView!
+    var redPoint: CGPoint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+//        redPoint = redView.center
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        redPoint = redView.center
     }
 
     @IBAction func handleGesture(pan: LZPanGestureRecognizer) {
         
+        let velocity = pan.velocityInView(view).y
+        
         // high stiffness means high acceleration (will help it stay under touch)
         if pan.state == .Ended {
-            redView.m_animate("center", to: view.center, stiffness:50, damping:25)
+            
+            redView.m_animate("center", to: redPoint, stiffness:50, damping:25)
         }
         
         if pan.state == .Changed {
             let point = pan.locationInView(view)
-            redView.m_animate("center", to: point, stiffness:50, damping:25)
+            
+            // Track velocity to detect pan direction
+            // You've set a max value, up and down, that the view is allowed to move to
+            if velocity > 0.0 {
+                print("down")
+                redView.m_animate("center", to: CGPointMake(redView.center.x, min(redPoint.y + 100.0,point.y)), stiffness:50, damping:25)
+            } else {
+                print("up")
+                redView.m_animate("center", to: CGPointMake(redView.center.x, max(redPoint.y - 100.0,point.y)), stiffness:50, damping:25)
+            }
         }
     }
 }
